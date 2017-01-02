5477ff902f59a99a2223b40fd260d923e880bd31
Vince <developer@mediavince.com>
2017-01-02 02:07:21 +0100

    set bootstrap var in config

--- mvcms/admin/defaults/_config.php
+++ mvcms/admin/defaults/_config.php
@@ -20,6 +20,8 @@ $google_jquery_ver = '1.4.2';// at date of edit 1.4.4 // produces '!' error
 // google_jquery enabled by default
 // $google_jquery = true;
 
+$bootstrap = false; // if true then make sure to set google_jquery = false !!
+
 /*----MENUFUNC-----*/
 $htaccess4sef = true; // this builds links as SEF, special htaccess needed
 $x1subok = true; // this allows menus under 1 and prevent top menu to go over 9
@@ -234,6 +236,24 @@ jQuery(document).ready(function($){
 		{src: \''.$mainurl.'images/_captcha.php\'}
 	);
  */
+
+if ($bootstrap) {
+	$stylesheet = '	
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+';
+	$javascript = '
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+';
+}
 
 $this_annee = date('Y');
 
