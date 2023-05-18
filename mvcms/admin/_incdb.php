72dbd784cf9dfe112646e5b1f65a10feb4c4573f
Vincent <vincent@leoxis.com>
2023-05-18 19:55:42 +0200

    chore:install forward back

--- mvcms/admin/_incdb.php
+++ mvcms/admin/_incdb.php
@@ -134,7 +134,7 @@ if (!isset($default_lg) || !in_array($default_lg,$array_supported_lg))
 
 ##############################################################################################
 include '_incerror.php';
-if (@file_exists($up.$safedir.'_params.php'))
+if (@file_exists($up.$safedir.'_params.php') and !stristr($_SERVER['PHP_SELF'], '_install.php'))
 {
     $connection = connect();
 } else {
